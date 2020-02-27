64d2cddb9bc5b358d7f6714e06cf6cf380bbe9ca
Charlie <cr646531@gmail.com>
2020-02-26 22:19:05 -0500

    changed the math to create an endless scroll of background images

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // static resources
 app.use(express.static(__dirname + "/public"));
--- src/index.js
+++ src/index.js
@@ -3,12 +3,31 @@ var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
 // update the canvas every 10ms
-var speed = 10;
+var speed = 1;
 var interval = setInterval(draw, speed);
+
+var background = document.getElementById("background");
+var background2 = document.getElementById("background2");
+
+var index_one = 0;
+var index_two = canvas.width * 2;
 
 // draw functions
 function draw() {
-	ctx.font = "16px Arial";
-	ctx.fillStyle = "black";
-	ctx.fillText(`Testing`, 10, 10);
+    ctx.drawImage(background, index_one, 0);
+    index_one--;
+
+    ctx.drawImage(background2, index_two, 0);
+    index_two--;
+
+    if(Math.abs(index_one) == canvas.width * 2) {
+        index_one = canvas.width * 2;
+    }
+
+    if(Math.abs(index_two) == canvas.width * 2) {
+        index_two = canvas.width * 2;
+    }
+
+    console.log(index_one);
+    
 }
